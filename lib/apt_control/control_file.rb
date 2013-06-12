87b0c779bd399e7364d77c8095c373570a5725c8
Nick Griffiths <nick@mediasp.com>
2013-06-12 11:48:02 +0100

    control file can be written back out

--- lib/apt_control/control_file.rb
+++ lib/apt_control/control_file.rb
@@ -10,12 +10,24 @@ module AptControl
       @logger = logger
       @watch_mutex = Mutex.new
       @path = path
-      inifile = IniFile.load(@path)
-      @distributions = parse!(inifile)
+      @distributions =
+        if File.exists?(@path)
+          inifile = IniFile.load(@path)
+          parse!(inifile)
+        else
+          []
+        end
     end
 
     def distributions
+      # not sure that this is strictly necessary - there is no true concurrency
+      # in MRI/YARV, but that doesn't mean a thread couldn't be interrupted half
+      # way through initialising a variable, does it?
       @watch_mutex.synchronize { @distributions }
+    end
+
+    def [](dist_name)
+      distributions.find {|d| d.name == dist_name }
     end
 
     def dump
@@ -26,6 +38,22 @@ module AptControl
             puts "  #{pr.package_name} #{pr.restriction} #{pr.version}"
           end
         end
+      end
+    end
+
+    def write
+      IniFile.new.tap do |inifile|
+        inifile.filename = @path
+        distributions.each do |distribution|
+          inifile[distribution.name] = distribution.inject({}) do |hash, rule|
+            hash.tap do |h|
+              # quote the restriction, as inifile doesn't do this for you
+              # https://github.com/TwP/inifile/pull/16
+              h[rule.package_name] = '"' + rule.restriction_string + '"'
+            end
+          end
+        end
+        inifile.write
       end
     end
 
@@ -45,8 +73,6 @@ module AptControl
       @watch_mutex.synchronize do
         @distributions = distributions
       end
-
-      @logger.info("...rebuilt")
     end
 
     # Watch the control file for changes, rebuilding
@@ -61,6 +87,7 @@ module AptControl
           @logger.info("Change to control file detected...")
           reload!
           yield if block_given?
+          @logger.info("...rebuilt")
         rescue => e
           @logger.error("Error reloading changes: #{e}")
           @logger.error(e)
@@ -126,17 +153,32 @@ module AptControl
       def includeable_to(available)
         available.select {|a| satisfied_by?(a) }
       end
+
+      def restriction_string
+        "#{@restriction} #{@version}"
+      end
+
+      # FIXME to_s should include the package name
+      alias :to_s :restriction_string
     end
 
     # represents a set of rules mapped to a particular distribution, i.e.
     # squeeze is a distribution
     class Distribution
+      include Enumerable
+
       def initialize(name, rules)
         @name = name
         @package_rules = rules
       end
       attr_reader :name
       attr_reader :package_rules
+
+      def each(&block)
+        (@package_rules || []).each do |rule|
+          yield(rule)
+        end
+      end
 
       # find a PackageRule by package name
       def [](package_name)
